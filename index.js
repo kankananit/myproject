03d1225952016332cf6a7d2442cdde3f0e5e860a
kankananit <112159467+kankananit@users.noreply.github.com>
2023-04-17 01:45:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const express=require('express')
+const collection=require("./mongo")
+const cors=require('cors')
+const app=express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors())
+
+app.get("/",cors(),(req,res)=>{
+
+})
+app.post("/",async(req,res)=>{
+    const{email,pasword}=req.body
+
+    try{
+        const check=await collection.findOne({email:email})
+        
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("not exist")
+        }
+    
+    }
+    catch(e){
+        res.json("not exist")
+    }
+
+})
+app.post("/Register",async(req,res)=>{
+    const{email,pasword}=req.body
+
+    const data={
+        email:email,
+        pasword:pasword
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+        
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("not exist")
+            await collection.insertMany([data])
+        }
+    
+    }
+    catch(e){
+        res.json("not exist")
+    }
+
+})
+
+app.listen(5000,()=>{
+    console.log("port connected")
+})
